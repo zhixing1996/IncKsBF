177052e88821330f3fd22811b0c5459fb25d4db0
zhixing1996 <jingmaoqiang1996@163.com>
2018-07-22 15:36:08 +0800

    Improve performance of SameGF(HsC)

--- Simulation/SameGF/HsC/SubFit.sh
+++ Simulation/SameGF/HsC/SubFit.sh
@@ -1,13 +1,132 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mkdir Combination
-cd Combination
-for (( i =1;i<101;i= i+1))
-do
-	mkdir Combination$i
-	cd Combination$i
-	cp ../../seed_combination/* . -rf
-	./ROOTCompile fit_ks_phase
-	./fit_ks_phase $i $i
-	cd ..
-done
+# Main driver to submit jobs 
+# Author JING Maoqiang <jingmq@ihep.ac.cn>
+# Created [2018-07-22 Sun 12:10] 
+
+if [ -d "Fit" ]; then 
+	rm Fit -rf
+	mkdir Fit
+fi
+
+if [ ! -d "Fit" ]; then 
+	mkdir Fit
+fi
+
+cd Fit
+
+option=$1
+
+case $option in
+	0.1) echo "Running on 1~10 combinations..."
+		for i in {1..10}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.2) echo "Running on 11~20 combinations..."
+		for i in {11..20}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.3) echo "Running on 21~30 combinations..."
+		for i in {21..30}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.4) echo "Running on 31~40 combinations..."
+		for i in {31..40}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.5) echo "Running on 41~50 combinations..."
+		for i in {41..50}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.6) echo "Running on 51~60 combinations..."
+		for i in {51..60}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.7) echo "Running on 61~70 combinations..."
+		for i in {61..70}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.8) echo "Running on 71~80 combinations..."
+		for i in {71..80}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	0.9) echo "Running on 81~90 combinations..."
+		for i in {81..90}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+	1.0) echo "Running on 91~100 combinations..."
+		for i in {91..100}
+		do
+			mkdir Combination$i
+			cd Combination$i
+			cp ../../seed_combination/* . -rf
+			./ROOTCompile fit_ks_phase
+			./fit_ks_phase $i $i
+			cd ..
+		done
+	;;
+
+esac
